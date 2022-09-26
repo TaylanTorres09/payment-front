6c401538ea14106cdf32c68d0830dd68f104e5b8
TaylanTorres09 <taylantorres@gmail.com>
2022-09-26 00:37:37 -0300

    Login complete

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
-function login() {
-    const email = document.getElementById("email").nodeValue;
-    console.log(email)
+async function login() {
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    if(email.value && password.value) {
+        const response = await fetch("http://localhost:8080/user/login", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({email: email.value, password: password.value}),
+        });
+        const content = await response.json();
+        console.log(content);
+    }
 }
